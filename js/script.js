775e7c5ae979709fa159dfd4e3ac604d50067441
Andrey <holmov70@mail.ru>
2020-05-24 18:08:33 +0300

    Это мой первый commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// let persone = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone['age']);
+
+// let arr = ['plun.png','orange.jpg','apple.bmp'];
+// console.log(arr[2]);
+
+let answer = prompt('Есть ли вам 18?', 'Yes');
+console.log(typeof(answer));
